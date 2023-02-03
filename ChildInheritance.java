9fcdf7f6ecafb4f1a22964a0c793b5f36fc4532d
unknown <kavidhas93@gmail.com>
2023-02-03 10:38:40 +0530

    java mAth operator program

---
+++ ChildInheritance.java
@@ -0,0 +1,14 @@
+package com.programs.java;
+
+public class ChildInheritance extends ParentInheritance {
+
+	public static void main(String[] args) {
+		
+		
+		ChildInheritance obj = new ChildInheritance();
+		obj.name();
+		
+
+	}
+
+}
